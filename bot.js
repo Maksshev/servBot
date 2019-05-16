ff472daa13be838ac3478f45ab0ec776da883b71
Maksym Shevchenko <maksym.shevchenko@icloud.com>
2019-05-17 01:36:22 +0300

    basic mvp

---
+++ bot.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const TelegramBot = require('node-telegram-bot-api');
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(bodyParser.json())
+
+
+const token = '884928727:AAGIow5SKhK1Nt6PThuwwQuHH32zpUuNWLA'
+const bot = new TelegramBot(token, {polling: true})
+
+
+const ids = []
+
+bot.onText(/\/r/, (msg, match) => {
+
+    const chatId = msg.chat.id;
+
+    ids.push(chatId)
+});
+
+
+app.get('/', (req, res) => {
+    res.send('')
+})
+
+app.post('/', (req, res) => {
+    ids.forEach(id => {
+        bot.sendMessage(id, JSON.stringify(req.body))
+    })
+
+    res.end()
+})
+
+
+app.listen(3000, () => {
+    console.log('Port 3000')
+})
+
+
